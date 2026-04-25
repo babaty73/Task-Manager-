3e826419ab94e1de637b74c2eb38db0d9d98b10b
imran <imranidris10999@gmail.com>
2026-04-25 07:22:56 -0700

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className=" flex justify-center bg-gray-900 items-center h-screen">
+      <h1 className="text-green-500 text-3xl font-bold">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
